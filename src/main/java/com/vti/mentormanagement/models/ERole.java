ac2511a4d354fa296953863fd92ff1f27b8f763c
Huylvph08000 <Huyyeumy31>
2021-03-06 01:01:27 +0700

    Initial commit

---
+++ src/main/java/com/vti/mentormanagement/models/ERole.java
@@ -0,0 +1,7 @@
+package com.vti.mentormanagement.models;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/vti/mentormanagement/MentorManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.mentormanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
